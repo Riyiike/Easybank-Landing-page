1c972866798a2a63cf67e6dae0f4fc751145b936
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-06-14 00:23:56 -0700

    mobile responsiveness

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function disappear() {
-    let mobileNav = document.getElementById("mobile-nav");
-    let close = document.getElementById("close");
-    let hamburger = document.getElementById("hamburger");
+    var mobileNav = document.getElementById("navbar");
+    var close = document.getElementById("close");
+    var hamburger = document.getElementById("hamburger");
     mobileNav.style.display = "none";
     close.style.display = "none";
     hamburger.style.display = "block";
   }
   function appear() {
-    let mobileNav = document.getElementById("mobile-nav");
-    let hamburger = document.getElementById("hamburger");
-    let close = document.getElementById("close");
+    var mobileNav = document.getElementById("navbar");
+    var hamburger = document.getElementById("hamburger");
+    var close = document.getElementById("close");
     mobileNav.style.display = "block";
     hamburger.style.display = "none";
     close.style.display = "block";
